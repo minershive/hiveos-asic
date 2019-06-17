977bccdb2663271fa03631199eea10c77efb25de
root <49.pasha@gmail.com>
2019-06-17 17:00:50 +0300

    FIX hiveon autorun, ADD setup API url to the bulk install

--- hive/hive-asic-net-installer/install.sh
+++ hive/hive-asic-net-installer/install.sh
@@ -22,7 +22,7 @@ echo -e "IPs count `echo "$IPS" | wc -l`"
 
 #sleep 1
 
-install_cmd="export PATH=$PATH:/hive/bin:/hive/sbin; export LD_LIBRARY_PATH=/hive/lib; cd /tmp && curl -L --insecure -s -O https://raw.githubusercontent.com/minershive/hiveos-asic/master/hive/bin/selfupgrade && FARM_HASH=$FARM_HASH sh selfupgrade master"
+install_cmd="export PATH=$PATH:/hive/bin:/hive/sbin; export LD_LIBRARY_PATH=/hive/lib; cd /tmp && curl -L --insecure -s -O https://raw.githubusercontent.com/minershive/hiveos-asic/master/hive/bin/selfupgrade && FARM_HASH=$FARM_HASH HIVE_HOST_URL=$HIVE_HOST_URL sh selfupgrade master"
 #install_cmd="pwd; ls" #for testing
 install_cmd="[ -e /hive ] && (echo Already_installed) || ($install_cmd)"
 
