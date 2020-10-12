0845dadd7e3711125238ec7c450640f1d7b2e220
j2h4u <ccc911.ngs.ru@gmail.com>
2020-10-12 16:07:22 +0700

    UPDATE hive-functions.sh
    * improved khs_to_human_friendly_hashrate() to properly show a hashrate below 1 kH/s

--- hive/bin/hive-functions.sh
+++ hive/bin/hive-functions.sh
@@ -11,7 +11,7 @@
 
 
 declare -r hive_functions_lib_mission='Client for ASICs: Oh my handy little functions'
-declare -r hive_functions_lib_version='0.44.0'
+declare -r hive_functions_lib_version='0.44.1'
 #                                        ^^ current number of public functions
 
 
@@ -541,7 +541,12 @@ function khs_to_human_friendly_hashrate {
 
 	if [[ "$hashrate_in_khs" == '0' || "$hashrate_in_khs" == 'null' ]]; then
 		echo '0 H/s'
+	elif [[ "$hashrate_in_khs" != *[Ee]* ]]; then
+		# a number without exponent
+		hs_decimal="$( scientific_to_decimal "${hashrate_in_khs}e3" )" # multiply by 1000 right there and then
+		big_decimal_to_human "$hs_decimal" 'H/s'
 	else
+		# a number with exponent, process with care
 		khs_decimal="$( scientific_to_decimal "$hashrate_in_khs" )"
 		(( hs_decimal = khs_decimal * 1000 ))
 		big_decimal_to_human "$hs_decimal" 'H/s'
